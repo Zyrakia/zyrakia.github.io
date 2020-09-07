6a95f8dc937079fdb6a0923285446229d950b899
Zyrakia <mailzyrakia@gmail.com>
2020-09-07 12:40:49 -0700

    Updated typecsript, fixed help pages display page numbers incorrectly.

--- scripts/commands/Help.js
+++ scripts/commands/Help.js
@@ -2,7 +2,7 @@ var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
             ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
         return extendStatics(d, b);
     };
     return function (d, b) {
@@ -71,7 +71,7 @@ var HelpCommand = /** @class */ (function (_super) {
                     case 0:
                         response = [];
                         commands = __spreadArrays(terminal.getCommands());
-                        pages = Math.ceil(commands.length / this.PER_PAGE);
+                        pages = Math.floor(commands.length / this.PER_PAGE);
                         if (args[0] && !isNaN(+args[0])) {
                             pageNum = +args[0];
                         }
